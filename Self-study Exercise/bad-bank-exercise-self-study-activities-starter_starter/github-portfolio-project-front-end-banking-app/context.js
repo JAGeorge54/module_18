207ac845016ca66bf0858647f240a7afbd60f638
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-12-04 15:09:58 -0500

    changed back to original code

--- Self-study Exercise/bad-bank-exercise-self-study-activities-starter_starter/github-portfolio-project-front-end-banking-app/context.js
+++ Self-study Exercise/bad-bank-exercise-self-study-activities-starter_starter/github-portfolio-project-front-end-banking-app/context.js
@@ -9,10 +9,11 @@ function Card(props){
       const txt = props.txtcolor ? ' text-' + props.txtcolor: ' text-white';
       return 'card mb-3 ' + bg + txt;
     }
-  
+    
+
     return (
       <div className={classes()} style={{maxWidth: "18rem"}}>
-        <div className="card-header" style={{backgroundColor: "#00AA9E"}}>{props.header}</div>
+        <div className="card-header">{props.header}</div>
         <div className="card-body">
           {props.title && (<h5 className="card-title">{props.title}</h5>)}
           {props.text && (<p className="card-text">{props.text}</p>)}
