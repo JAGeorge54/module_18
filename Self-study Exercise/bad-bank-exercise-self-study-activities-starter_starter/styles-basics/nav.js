8ff25ca47126fd6296f52a00d0a49d23d1fb5aa4
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-12-04 09:15:11 -0500

    changed to dark theme navbar

--- Self-study Exercise/bad-bank-exercise-self-study-activities-starter_starter/styles-basics/nav.js
+++ Self-study Exercise/bad-bank-exercise-self-study-activities-starter_starter/styles-basics/nav.js
@@ -1,15 +1,17 @@
 function Nav() {
   return(
-    <ul className="nav">
-      <li className="nav-item">
-        <Link className="nav-link" to="/">Home</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/about/">About</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/Products/">Products</Link>
-      </li>
-    </ul>
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+      <ul className="nav">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/about/">About</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/Products/">Products</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
