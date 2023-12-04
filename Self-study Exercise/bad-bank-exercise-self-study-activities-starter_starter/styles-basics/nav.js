c5fd08586b58b30308d2646fbb749d318730db7b
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-12-04 09:39:55 -0500

    added bank pic and brand img

--- Self-study Exercise/bad-bank-exercise-self-study-activities-starter_starter/styles-basics/nav.js
+++ Self-study Exercise/bad-bank-exercise-self-study-activities-starter_starter/styles-basics/nav.js
@@ -1,6 +1,10 @@
 function Nav() {
   return(
-    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+    <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+      <a className="navbar-brand" href="#">
+        <img src="./bankPic.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+        BadBank
+      </a>
       <ul className="nav">
         <li className="nav-item">
           <Link className="nav-link" to="/">Home</Link>
